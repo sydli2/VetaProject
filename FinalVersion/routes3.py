3635dd6af53dd4073987bb89987a151dff14efd3
sydli2 <79809811+sydli2@users.noreply.github.com>
2021-05-17 03:52:36 -0500

    Update routes3.py

--- FinalVersion/routes3.py
+++ FinalVersion/routes3.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[11]:
+
 
 
 import flask
@@ -97,7 +97,7 @@ def get_vol_surface_2(symbol,S,r):
     for ind_T in range(len(option_dates)):
         df=quote.option_chain(option_dates[ind_T]).calls
         
-        if df.shape[0]==0:                            #if system says empty dataframe, go to the next iteration
+        if df.shape[0]==0:                            # if system says empty dataframe, go to the next iteration
             #print(option_dates[ind_T]+ "is skipped")
             continue
             
@@ -306,8 +306,6 @@ if __name__ == '__main__':
 
 # @app.route('/',methods=['POST'])
 
-
-# In[ ]:
 
 
 
